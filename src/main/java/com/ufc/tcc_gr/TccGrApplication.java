c812390f276b9faccb17212568896a51cab45111
rabel-o <gustavo.rabelo@alu.ufc.br>
2026-03-27 02:52:02 -0300

    Implement initial project structure and setup

---
+++ src/main/java/com/ufc/tcc_gr/TccGrApplication.java
@@ -0,0 +1,13 @@
+package com.ufc.tcc_gr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccGrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccGrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufc/tcc_gr/TccGrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc.tcc_gr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccGrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
